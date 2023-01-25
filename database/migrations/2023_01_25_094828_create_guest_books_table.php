59ad61fb82f0ecf988889372f73f7382bf188d00
Akbar Hamaminatu <akbar@ngoding.id>
2023-01-25 17:37:21 +0700

    feat: Add guestbook table and model

---
+++ database/migrations/2023_01_25_094828_create_guest_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guest_books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('date');
+            $table->string('class');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guest_books');
+    }
+};
