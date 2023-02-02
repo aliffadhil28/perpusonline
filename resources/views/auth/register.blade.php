fa2b779d70da61359c3c527591bec730355a1be7
MrNo0ne12 <alif70662@gmail.com>
2023-02-02 17:12:17 +0700

    logo title regist,login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,86 +1,10 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection --}}
 <!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="x-icon" href="{{ asset('img/logo.png') }}">
     <title>Registrasi Perpus Online</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
@@ -205,46 +129,11 @@
                                 name="password_confirmation" required autocomplete="new-password"
                                 placeholder="Masukkan kembali password">
                         </div>
-                        {{-- <div class="form-group">
-                            <label for="alamat" class="form-label">Alamat</label>
-                            <input type="text" class="form-control" id="alamat" aria-describedby="emailHelp"
-                                placeholder="Masukkan Nomormu">
-                        </div> --}}
-                        {{-- <div class="form-group">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
-                                placeholder="Masukkan email kamu">
-                        </div> --}}
-                        {{-- <div class="form-group">
-                            <label for="username" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="username" aria-describedby="emailHelp"
-                                placeholder="Masukkan username">
-                        </div> --}}
-                        {{-- <div class="form-group">
-                            <label for="nik" class="form-label">NIK</label>
-                            <input type="text" class="form-control" id="nik" aria-describedby="emailHelp"
-                                placeholder="Masukkan NIK">
-                        </div> --}}
-                        {{-- <div class="form-group">
-                            <label for="notelp" class="form-label">Nomor Telepon</label>
-                            <input type="text" class="form-control" id="notelp" aria-describedby="emailHelp"
-                                placeholder="Ex : +6281234567890">
-                        </div> --}}
-                        {{-- <div class="form-group">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" aria-describedby="emailHelp"
-                                placeholder="Masukkan password">
-                        </div>
-                        <div class="form-group">
-                            <label for="username" class="form-label">Konfirmasi Password</label>
-                            <input type="password" class="form-control" id="passwordk" aria-describedby="emailHelp"
-                                placeholder="Masukkan kembali password">
-                        </div> --}}
                         <div class="form-group">
                             <button type="submit" class="sign-in btn btn-primary">
                                 {{ __('Register') }}
                             </button>
-                            
+
                         </div>
                             <div class="form-group">
                             <a href="{{ route('login') }}">Kembali</a>
